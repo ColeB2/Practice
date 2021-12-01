e769cb1ab733c32401643566100bd6e46ea81a81
Cole <cebCole@gmail.com>
2021-12-01 15:24:56 -0600

    revert spacing changes

--- resources/js/main.js
+++ resources/js/main.js
@@ -58,41 +58,41 @@ class Main {
 	}
 }
 
-export function main_obj = {
-	const mainGame = new Main();
-	mainGame.initializeBoard();
-	const pauseButton = document.getElementById('pause')
-	const rangeSlider = document.getElementById('myRange');
-	const output = document.getElementById('sliderValue');
-	output.innerHTML = rangeSlider.value;
+
+const mainGame = new Main();
+mainGame.initializeBoard();
+const pauseButton = document.getElementById('pause')
+const rangeSlider = document.getElementById('myRange');
+const output = document.getElementById('sliderValue');
+output.innerHTML = rangeSlider.value;
 	
-	rangeSlider.oninput = function() {
-		output.innerHTML = this.value;
-		mainGame.delay = this.value;
-	}
+rangeSlider.oninput = function() {
+	output.innerHTML = this.value;
+	mainGame.delay = this.value;
+}
 	
-	function pauseLoop() {
-		if (mainGame.isRunning) {
-			pauseButton.innerText = 'Start';
-			pauseButton.classList.remove('button-paused')
-		} else {
-			pauseButton.innerText = 'Pause';
-			pauseButton.classList.add('button-paused')
-		}
-		mainGame.isRunning = !mainGame.isRunning;
-		mainGame.runGame()
+function pauseLoop() {
+	if (mainGame.isRunning) {
+		pauseButton.innerText = 'Start';
+		pauseButton.classList.remove('button-paused')
+	} else {
+		pauseButton.innerText = 'Pause';
+		pauseButton.classList.add('button-paused')
 	}
+	mainGame.isRunning = !mainGame.isRunning;
+	mainGame.runGame()
+}
 	
-	function resetBoard() {
-		document.getElementById('reset')
-		mainGame.Board.resetState()
-		mainGame.updateGame();
-	}
+function resetBoard() {
+	document.getElementById('reset')
+	mainGame.Board.resetState()
+	mainGame.updateGame();
+}
 	
-	function randomBoard() {
-		mainGame.Board.randomState();
-		mainGame.updateGame();
-	}
+function randomBoard() {
+	mainGame.Board.randomState();
+	mainGame.updateGame();
+}
 
 function startLoop() {
 	
